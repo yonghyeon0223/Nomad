8539256caa2be16addfdf0e24a9b809b0a92503d
Isaac <yonghyeon0223@gmail.com>
2022-07-09 20:41:58 +0900

    multiple requesting

---
+++ Webpage.py
@@ -0,0 +1,130 @@
+from multiprocessing.sharedctypes import Value
+import requests
+from bs4 import BeautifulSoup
+import time
+
+class EndOfRequestException(Exception):
+    def __init__(self):
+        super().__init__("Request Process Finished!")
+
+def extract_num(line):
+    line = list(line)
+    num = ""
+    for chr in line:
+        if chr in "0123456789":
+            num += chr
+        else:
+            return int(num)
+    return int(num)
+
+def utf_encode(text):
+    utf_text = str(text).encode("utf-8")
+    return str(utf_text)
+
+
+class Webpage:
+    def __init__(self, url, pagination_info, search_info):
+        self.url = url
+        self.soup = self.get_soup()
+
+        self.pagination = list()
+        self.pagination_info = pagination_info
+
+        self.current_page = self.get_current_page() # current page index
+        self.find_pagination() # stores pages' index and link in a list
+        self.next_page_link = self.get_next_page_link() # next page url
+
+        self.search_info = search_info
+    
+    def get_soup(self):
+        while True:
+            req = requests.get(self.url)
+            soup = BeautifulSoup(req.text, "html.parser")
+            if req != None and soup != None:
+                break
+            print("requesting again..")
+        return soup
+            
+
+    def find_pagination(self):
+        location = self.pagination_info.get("location")
+        classname = self.pagination_info.get("classname")
+        query_line = self.pagination_info.get("query_line")
+
+        try:
+            pagination = self.soup.find(location, {"class" : classname})
+            pages =  pagination.find_all("a")
+        
+            for page in pages:
+                self.pagination.append(Pagination(page.get("href"), query_line))
+        except AttributeError as e:
+            print(self.url, self.pagination_info)
+            print(e)
+
+    def get_current_page(self):
+        query_line = self.pagination_info["query_line"]
+        try:
+            start = int(self.url.index(query_line)) + len(query_line)
+            return extract_num(self.url[start:])
+        except ValueError as e:
+            return 0
+    
+    def get_next_page_link(self):
+        next_p = self.pagination[-1]
+        query_line = self.pagination_info.get("query_line")
+        if self.current_page < next_p.location:
+            if query_line in self.url:
+                return self.url.replace(query_line + str(self.current_page), query_line + str(next_p.location))
+            else:
+                return (self.url + next_p.page_link)
+        else:
+            raise EndOfRequestException
+
+    def search_element(self, search_info):
+        location = search_info["tag_location"]
+        classname = search_info["classname"]
+
+        found_ls = self.soup.find_all(location, {"class" : classname})
+        result = list()
+
+        for found in found_ls:
+            string_res = found.text
+            try:
+                result.append(utf_encode(string_res)[2:-1])
+            except UnicodeDecodeError as e:
+                result.append("encoding failed :(")
+        return result
+
+    def search_elements(self):
+        result_ls = list()
+        for search in self.search_info:
+            result_ls.append(self.search_element(search))
+        return result_ls
+
+    def get_progress(self, i, freq=1):
+        if i % freq != 0:
+            return ""
+        line = "----------WEBPAGE INFORmATION----------"\
+            + "\nURL:" + self.url\
+            + "\nCURRENT PAGE:" + str(self.current_page)\
+            + "\nNEXT PAGE:" + self.next_page_link\
+            + "Pagination:\n"
+        
+        for page in self.pagination:
+            line += page.toString()
+        line += "==========================================\n\n"
+        return line
+
+
+class Pagination:
+    def __init__(self, page_link, query_line):
+        try:
+            start = int(page_link.index(query_line)) + len(query_line)
+            self.location = extract_num(page_link[start:])
+        except ValueError as e:
+            self.location = 0
+        self.page_link = page_link
+    
+    def toString(self):
+        return "\tpage location: " + str(self.location) + " page_link: " + self.page_link + "\n"
+
---
+++ multi-request.py
@@ -0,0 +1,68 @@
+from Webpage import Webpage
+import csv
+
+
+def progress_file_setup(filename):
+    with open(filename, "w") as f:
+        f.write("")
+
+
+def result_file_setup(filename, description_ls):
+    with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(description_ls)
+
+
+def write_progress(filename, content):
+    with open(filename, "a") as f:
+        f.write(str(content))
+
+
+def write_result(filename, result_ls):
+    length = len(result_ls[0])
+
+    for result in result_ls:
+        # print(len(result))
+        assert(len(result) == length)
+        
+    with open(filename, "a", newline="") as f:
+        writer = csv.writer(f)
+        for i in range(length):
+            row = list()
+            for result in result_ls:
+                row.append(result[i])
+            writer.writerow(row)
+
+
+def run(url, pagination_info, search_info, progress_file, result_file, contain_num):
+    progress_file = "query_result/" + progress_file
+    result_file =  "query_result/" + result_file
+    progress_file_setup(progress_file)
+    result_file_setup(result_file, search_info[0])
+
+    search_info = search_info[1:]
+    Current_page = Webpage(url, pagination_info, search_info)
+
+    i = 0
+    while True:
+        write_progress(progress_file, Current_page.get_progress(i))
+        write_result(result_file, Current_page.search_elements())
+        Current_page = Webpage(Current_page.next_page_link, pagination_info, search_info)
+        print(f"Over {(i+1)*contain_num} Queries Processed")
+        i += 1
+
+
+if __name__ == "__main__":
+# INDEED COOK JOB SEARCH
+    # URL and PAGINATION
+    url = "https://au.indeed.com/jobs?q=cook&vjk=6493e1124836c3fc"
+    pagination = dict(location="div", classname="pagination", query_line="start=")
+    # QUERY SEARCH
+    job_search = dict(tag_location="h2", classname="jobTitle")
+    company_search = dict(tag_location="span", classname="companyName")
+    compnay_location = dict(tag_location="div", classname="companyLocation")
+    search_info = (["Job Title", "Company", "Location"], job_search, company_search, compnay_location)
+    # RUN PROGRAM
+    run(url, pagination, search_info, "INDEED-process.txt", "INDEED-result.csv", 15)
+
+
